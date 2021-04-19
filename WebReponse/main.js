744cc06b9478472d213a61511519197b40a49ff3
nguyenduc97 <haiduc1120@gmail.com>
2021-04-19 15:35:29 +0700

    a

---
+++ WebReponse/main.js
@@ -0,0 +1,6 @@
+function btntoggle() {
+	var navbar = document.getElementById("navbar");
+	var toggle = document.getElementById("toggle");
+	navbar.classList.toggle('active');
+	toggle.classList.toggle('active');
+}
